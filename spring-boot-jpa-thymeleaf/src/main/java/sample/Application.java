8c4d67392a90564fe8916b686fe9f6ffd300eec6
Jason <Jason@DESKTOP-6A7GNFU>
2016-03-14 09:49:35 +0800

    Spring Boot jpa thymeleaf

---
+++ spring-boot-jpa-thymeleaf/src/main/java/sample/Application.java
@@ -0,0 +1,19 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动类 
+ * @author yangqijie
+ * @version 2016年3月14日
+ */
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
